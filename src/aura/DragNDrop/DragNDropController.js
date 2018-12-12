4f4de14d9edec94c3e692242191d8146ad5047a4
emi <emiliano.gonzalez@modelit.xyz>
2018-12-12 11:33:42 -0300

    Assignment Completed

---
+++ src/aura/DragNDrop/DragNDropController.js
@@ -0,0 +1,40 @@
+({  
+    // Load current profile picture
+    onInit: function(component) {
+        var action = component.get("c.getProfilePicture"); 
+        action.setParams({
+            parentId: component.get("v.recordId"),
+        });
+        action.setCallback(this, function(a) {
+            var image = a.getReturnValue();
+            console.log(image);
+            if (image != null && image.Id != null) {
+                component.set("v.image", '/sfc/servlet.shepherd/version/download/'+image.Id);
+                component.set("v.renderNew", true);
+            }
+            
+        });
+        $A.enqueueAction(action); 
+    },
+    
+    onDragOver: function(component, event) {
+        event.preventDefault();
+    },
+
+    onDrop: function(component, event, helper) {
+
+        event.stopPropagation();
+        event.preventDefault();
+        event.dataTransfer.dropEffect = 'copy';
+        var files = event.dataTransfer.files;
+        if (files.length>1) {
+            return alert("You can only upload one profile picture");
+        }
+        helper.readFile(component, helper, files[0]);
+    },
+
+    deletePicture: function(component, event, helper) {
+        helper.delete(component);
+    }
+    
+})
---
+++ src/aura/DragNDrop/DragNDropHelper.js
@@ -0,0 +1,43 @@
+({
+    readFile: function(component, helper, file) {
+        if (!file) return;
+        if (!file.type.match(/(image.*)/)) {
+            return alert('Image file not supported');
+        }
+        var reader = new FileReader();
+        reader.onloadend = function() {
+            var dataURL = reader.result;
+            console.log(dataURL);
+            component.set("v.image", dataURL);
+            helper.upload(component, file, dataURL.match(/,(.*)$/)[1]);
+        };
+        reader.readAsDataURL(file);
+    },
+    
+    upload: function(component, file, base64Data) {
+        var action = component.get("c.saveTheFile"); 
+        action.setParams({
+            parentId: component.get("v.recordId"),
+            fileName: file.name,
+            base64Data: base64Data, 
+            contentType: file.type
+        });
+        action.setCallback(this, function(a) {
+            component.set("v.renderNew", true);
+        });
+        $A.enqueueAction(action); 
+    },
+
+    delete: function(component) {
+        var action = component.get("c.deleteTheFile"); 
+        action.setParams({
+            parentId: component.get("v.recordId"),
+        });
+        action.setCallback(this, function(a) {
+            component.set("v.renderNew", false);
+            component.set("v.image", "https://i.imgur.com/1RM15yn.jpg");
+        });
+        $A.enqueueAction(action); 
+    }
+
+})
