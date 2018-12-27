1fb0c47e04d1af591cbb864611dc62811a7c5f16
Emiliano Gonzalez <44619028+gonzalezemiliano@users.noreply.github.com>
2018-12-27 09:47:49 -0300

    Assignment Completed (#1)
    * Assignment Completed
    
    * added git ignore
    
    * added git ignore
    
    * change return statement in methods
    
    * Change review comments
    
    * Add metadata and class header
    
    * Check Indentation
    
    * Update DragNDropControllerTest.cls
    
    * Update DragNDropControllerTest.cls
    
    * Modifications from review comments
    
    * Change Apex Class and package.xml
    
    * Update Apex Class and Test Class
    
    * Handle exceptions in classes
    
    * handle client side exceptions
    
    * Delete DragNDropController.old.cls
    
    * Delete DragNDropControllerTest.cls
    
    * Delete DragNDropControllerTest.cls-meta.xml
    
    * Edit TestDragNDropController
    
    * Check indentation
    
    * Edit deleteTheFile and testGetProfilePicture
    
    * Correct Indentation
    
    * Change API version to 44.0 in XML files
    
    * Edit package.xml
    
    * Edit package.xml
    
    * Add more comments inside Apex Class
    
    * Show apex exception in Cmp with showToast
    
    * Fix changes assigned in pull review
    
    * Remove console logs from Controller and Helper
    
    * Fix issues from comments review
    
    * Add custom message for image state
    
    * Fix issues from comments review
    
    * Fix issues from comments
    
    * Correct Indentation
    
    * Correct Indentation
    
    * Correct Indentation
    
    * Wrap layout inside a lightning card
    
    * Hide message when image uploaded
    
    * Change message font
    
    * Add databaseRollback in catch exception
    
    * Show custom error message on toast
    
    * Add errorToast function so as to reduce code
    
    * Add else-if statement in upload helper method
    
    * Edit TestDragNDropController
    
    * Edit TestDragNDropController
    
    * Edit TestDragNDropController
    
    * Update TestDragNDropController.cls
    
    * Create TestDataFactory
    
    * Edit TestDataFactory and TestDragNDropController

---
+++ src/aura/DragNDropCmp/DragNDropCmpController.js
@@ -0,0 +1,47 @@
+({
+    // Load current profile picture.
+    onInit: function(component) {
+        let action = component.get("c.getProfilePicture");
+        action.setParams({
+            parentId: component.get("v.recordId"),
+        });
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+
+            if (state == "SUCCESS") {
+                let image = response.getReturnValue();
+
+                if (image != null && image.Id != null) {
+                    component.set("v.image", '/sfc/servlet.shepherd/version/download/' + image.Id);
+                    component.set("v.message", "");
+                    component.set("v.renderDelete", true);
+                }
+            } else if (state == "ERROR") {
+                helper.errorToast(response.getError()[0].message);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    onDragOver: function(component, event) {
+        event.preventDefault();
+    },
+
+    onDrop: function(component, event, helper) {
+        event.stopPropagation();
+        event.preventDefault();
+        event.dataTransfer.dropEffect = 'copy';
+        let files = event.dataTransfer.files;
+
+        if (files.length > 1) {
+            return alert("You can only upload one profile picture");
+        }
+
+        helper.readFile(component, files[0]);
+    },
+
+    deletePicture: function(component, event, helper) {
+        helper.delete(component);
+    }
+
+})
---
+++ src/aura/DragNDropCmp/DragNDropCmpHelper.js
@@ -0,0 +1,71 @@
+({
+    readFile: function(component, file) {
+        if (!file) return;
+
+        if (!file.type.match(/(image.*)/)) {
+            return alert('Image file not supported');
+        }
+
+        let self = this;
+        let reader = new FileReader();
+        reader.onloadend = function() {
+            let dataURL = reader.result;
+            component.set("v.image", dataURL);
+            self.upload(component, file, dataURL.match(/,(.*)$/)[1]);
+        };
+        reader.readAsDataURL(file);
+    },
+
+    upload: function(component, file, base64Data) {
+        let action = component.get("c.saveFile");
+        action.setParams({
+            parentId: component.get("v.recordId"),
+            fileName: file.name,
+            base64Data: base64Data,
+            contentType: file.type
+        });
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+
+            if (state == "SUCCESS") {
+                component.set("v.renderDelete", true);
+                component.set("v.message", "Image uploaded");
+            } else if (state == "ERROR") {
+                this.errorToast(response.getError()[0].message);
+            }
+        });
+        component.set("v.message", "Uploading...");
+        $A.enqueueAction(action);
+    },
+
+    delete: function(component) {
+        let action = component.get("c.deleteFile");
+        action.setParams({
+            parentId: component.get("v.recordId"),
+        });
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+
+            if (state == "SUCCESS") {
+                component.set("v.renderDelete", false);
+                component.set("v.image", "https://i.imgur.com/1RM15yn.jpg");
+                component.set("v.message", "Image deleted");
+            } else if (state == "ERROR") {
+                this.errorToast(response.getError()[0].message);
+            }
+
+        });
+        $A.enqueueAction(action);
+    },
+
+    errorToast: function(message){
+        let toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            title: 'Error',
+            type: 'error',
+            message: message
+            });
+        toastEvent.fire();
+    }
+
+})
